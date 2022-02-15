f75772e41a5b41526e239dff4f59a96ffdc72a74
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2022-02-16 01:49:44 +0400

    Update Transaction Gas Fees.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- app/scripts/controllers/transactions/index.js
+++ app/scripts/controllers/transactions/index.js
@@ -2221,6 +2221,11 @@ export default class TransactionController extends EventEmitter {
         this.updateEventFragment(id, { properties, sensitiveProperties });
         this.finalizeEventFragment(`transaction-submitted-${txMeta.id}`);
         break;
+      case TRANSACTION_EVENTS.GAS_FEES_UPDATED:
+        id = `transaction-gas-fees-updated-${txMeta.id}`;
+        this.updateEventFragment(id, { properties, sensitiveProperties });
+        this.finalizeEventFragment(`transaction-gas-fees-updated-${txMeta.id}`);
+        break;
       default:
         break;
     }
